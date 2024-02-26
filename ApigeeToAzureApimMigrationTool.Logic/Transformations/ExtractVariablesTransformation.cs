cd272d08cf98a9609ead77162f96ec0b2e63e269
Pooyan Fekrati <pfekrati@microsoft.com>
2024-02-26 11:43:40 -0500

    added support for query param and xml pay load

--- ApigeeToAzureApimMigrationTool.Logic/Transformations/ExtractVariablesTransformation.cs
+++ ApigeeToAzureApimMigrationTool.Logic/Transformations/ExtractVariablesTransformation.cs
@@ -2,11 +2,14 @@
 using ApigeeToAzureApimMigrationTool.Core.Interface;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection.PortableExecutable;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Xml;
 using System.Xml.Linq;
 
 namespace ApigeeToAzureApimMigrationTool.Service.Transformations
@@ -109,19 +112,82 @@ namespace ApigeeToAzureApimMigrationTool.Service.Transformations
                                  $"string headerValue = \"\"; string pattern=\"{patternValue}\"" +
                                 $"headerValue = context.Request.Headers.GetValueOrDefault(\"{headerName}\");" +
                                 $"headerValue = headerValue.Replace(pattern.Replace(\"{variablePattern}\", \"\"),\"\");" +
+                                "return headerValue;" +
                                 "}";
                         string apimVariableName = string.IsNullOrEmpty(variablePrefix) ? variableName : $"{variablePrefix}.{variableName}";
                         policies.Add(new XElement("set-variable", new XAttribute("name", apimVariableName), new XAttribute("value", apimExpression)));
                     }
                 }
             }
+            //TODO: support for multi params policies are not yet implemented
             else if (element.Elements("QueryParam") != null)
             {
-
+                string queryParamName = element.Element("QueryParam").Attribute("name").Value;
+                string patternValue = element.Element("QueryParam").Element("Pattern").Value;
+                bool IgnoreCaseInPattern = Convert.ToBoolean(element.Element("QueryParam").Element("Pattern").Attribute("ignoreCase").Value);
+                string patternRegex = @"{(.*?)}";
+                foreach (Match match in Regex.Matches(patternValue, patternRegex))
+                {
+                    if (match.Success && match.Groups.Count > 0)
+                    {
+                        string variableName = match.Groups[1].Value;
+                        string variablePattern = match.Groups[0].Value;
+                        string apimExpression = "@{" +
+                                 $"string queryParamValue = \"\"; string pattern=\"{patternValue}\"" +
+                                $"queryParamValue = context.Request.Url.Query.GetValueOrDefault(\"{queryParamName}\");" +
+                                $"queryParamValue = queryParamValue.Replace(pattern.Replace(\"{variablePattern}\", \"\"),\"\");" +
+                                "return queryParamValue;" +
+                                "}";
+                        string apimVariableName = string.IsNullOrEmpty(variablePrefix) ? variableName : $"{variablePrefix}.{variableName}";
+                        policies.Add(new XElement("set-variable", new XAttribute("name", apimVariableName), new XAttribute("value", apimExpression)));
+                    }
+                }
             }
             else if (element.Elements("XMLPayload") != null)
             {
+                var payloadElement = element.Element("XMLPayload");
+                string namespaceName = payloadElement.Element("Namespaces").Element("Namespace").Value;
+                string namespaceNamePrefix = payloadElement.Element("Namespaces").Element("Namespace").Attribute("prefix").Value;
 
+                foreach (var variableElement in payloadElement.Elements("Variable"))
+                {
+                    string translatedExpression = string.Empty;
+                    if (isSourceVariable)
+                    {
+                        translatedExpression = "context.Variables.GetValueOrDefault<string>(\"" + sourceName + "\")";
+                    }
+                    else
+                    {
+                        translatedExpression = $"context.{sourceName}.Body.As<string>(preserveContent: true)";
+                    }
+
+                    var variableName = variableElement.Attribute("name").Value;
+                    var xmlPath = variableElement.Element("XPath").Value;
+                    string apimExpression;
+                    if (!string.IsNullOrEmpty(namespaceName) && !string.IsNullOrEmpty(namespaceNamePrefix))
+                    {
+                        apimExpression = "@{" +
+                               "XmlDocument doc = new XmlDocument();" +
+                               $"doc.LoadXml({translatedExpression});" +
+                               "XmlNamespaceManager nsmgr = new XmlNamespaceManager(doc.NameTable);" +
+                               $"nsmgr.AddNamespace (\"{namespaceNamePrefix}\",\"{namespaceName}\");" +
+                               "var extractedValue = doc.SelectSingleNode(\"" + xmlPath + "\", nsmgr).Value;" +
+                               "return extractedValue;" +
+                               "}";
+                    }
+                    else
+                    {
+                        apimExpression = "@{" +
+                                "XmlDocument doc = new XmlDocument();" +
+                                $"doc.LoadXml({translatedExpression});" +
+                                "var extractedValue = doc.SelectSingleNode(\"" + xmlPath + "\").Value;" +
+                                "return extractedValue;" +
+                                "}";
+                    }
+                    string apimVariableName = string.IsNullOrEmpty(variablePrefix) ? variableName : $"{variablePrefix}.{variableName}";
+                    policies.Add(new XElement("set-variable", new XAttribute("name", apimVariableName), new XAttribute("value", apimExpression)));
+                    _policyVariables.Add(new KeyValuePair<string, string>(policyName, variableName));
+                }
             }
 
             return policies;
