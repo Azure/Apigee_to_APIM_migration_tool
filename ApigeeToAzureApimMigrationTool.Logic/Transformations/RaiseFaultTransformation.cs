e6d1331d93a37a5bead178a7bfc7c3cb97f61348
Pooyan Fekrati <pfekrati@microsoft.com>
2024-02-27 10:15:19 -0500

    created the transformer class

---
+++ ApigeeToAzureApimMigrationTool.Logic/Transformations/RaiseFaultTransformation.cs
@@ -0,0 +1,18 @@
+﻿using ApigeeToAzureApimMigrationTool.Core.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ApigeeToAzureApimMigrationTool.Service.Transformations
+{
+    public class RaiseFaultTransformation : IPolicyTransformation
+    {
+        public Task<IEnumerable<XElement>> Transform(XElement apigeePolicy, string apigeePolicyName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
