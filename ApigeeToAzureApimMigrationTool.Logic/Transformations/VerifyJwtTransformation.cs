5de1333d9a18e6ceb166ddd507faafae88a364d4
Pooyan Fekrati <106823811+pfekrati@users.noreply.github.com>
2024-04-02 11:26:47 -0400

    updated verifyjwt token policy transformation (#49)

--- ApigeeToAzureApimMigrationTool.Logic/Transformations/VerifyJwtTransformation.cs
+++ ApigeeToAzureApimMigrationTool.Logic/Transformations/VerifyJwtTransformation.cs
@@ -26,17 +26,27 @@ namespace ApigeeToAzureApimMigrationTool.Service.Transformations
 
             return Task.FromResult(apimPolicies.AsEnumerable());
         }
-        private XElement ValidateJwt(XElement element, string condition = null)
+        private XElement ValidateJwt(XElement element)
         {
             var token = element.Element("Source").Value;
-            var key = element.Element("PublicKey").Element("Value").Attribute("ref").Value;
+            var key = element.Element("PublicKey")?.Element("Value")?.Attribute("ref")?.Value;
+            if (key == null)
+                throw new NotSupportedException("VerifyJWT policy without defined public key is not supported");
 
-            var decryptionKeyelement = new XElement("decryption-keys");
-            decryptionKeyelement.Add(new XElement("key", $"@(context.Variables.GetValueOrDefault<string>(\"{key}\"))"));
+            var decryptionKeyelement = new XElement("issuer-signing-keys");
+            decryptionKeyelement.Add(new XElement("key", $"@(context.Variables.GetValueOrDefault<string>(\"{key}\",\"\"))"));
             string outputVariableName = $"{token}_validated";
-            var newPolicy = new XElement("validate-jwt", new XAttribute("token-value", $"@(context.Variables.GetValueOrDefault<string>(\"{token}\"))"), decryptionKeyelement,
+            var newPolicy = new XElement("validate-jwt", new XAttribute("token-value", $"@(context.Variables.GetValueOrDefault<string>(\"{token}\",\"\"))"), decryptionKeyelement,
                 new XAttribute("output-token-variable-name", outputVariableName));
             _policyVariables.Add(new KeyValuePair<string, string>(element.Attribute("name").Value, outputVariableName));
+
+            if(element.Element("IgnoreInvalidToken") != null)
+            {
+                newPolicy.Add(new XAttribute("ignore-error", "true"));
+            }
+
+
+
             return newPolicy;
         }
 
